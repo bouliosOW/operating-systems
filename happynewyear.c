4b4a22038bae1bcd80194dd1bea559edd163f1fc
bouliosOW <94017448+bouliosOW@users.noreply.github.com>
2025-01-15 14:58:27 -0500

    Add files via upload

---
+++ happynewyear.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(){
+	int counter = 10;
+
+    while (counter > 0){
+        printf("%d\n", counter);
+        counter--;
+    }
+    printf("Happy New Year!\n");
+
+	return 0;
+}
