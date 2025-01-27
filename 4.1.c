d3d48f2f2dff57a60b38360bb4bffdf314586f03
bouliosOW <94017448+bouliosOW@users.noreply.github.com>
2025-01-26 22:38:29 -0500

    Add files via upload

--- 4.1.c
+++ 4.1.c
@@ -6,4 +6,8 @@ void main(){
     fptr = fopen("hello.txt", "w");
     fprintf(fptr, "Hello,\n How are you?\n My name is Adam\n What is your name?");
     
+    fptr = fopen("data to be writted", "a");
+
+    
+    fclose(fptr);
 }
