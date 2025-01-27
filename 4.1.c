fbbf3e1f5dbac6458b92281e8d3897fa9814a126
bouliosOW <94017448+bouliosOW@users.noreply.github.com>
2025-01-26 22:39:57 -0500

    Update 4.1.c

--- 4.1.c
+++ 4.1.c
@@ -5,9 +5,6 @@ void main(){
 
     fptr = fopen("hello.txt", "w");
     fprintf(fptr, "Hello,\n How are you?\n My name is Adam\n What is your name?");
-    
-    fptr = fopen("data to be writted", "a");
 
-    
     fclose(fptr);
 }
