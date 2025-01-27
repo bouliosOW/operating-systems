83fb14a0c3df2c09a0f5ea4f3aaf28de86fecbd9
bouliosOW <94017448+bouliosOW@users.noreply.github.com>
2025-01-26 20:37:08 -0500

    Create 1.3.c

---
+++ 1.3.c
@@ -0,0 +1,9 @@
+num_items = 50
+cost_per_item = 9.99
+
+
+total_cost = num_items * cost_per_item;
+  
+printf("Number of items: %d", num_items);
+printf("Cost per item: %.2f", cost_per_item);
+printf("Total cost: $%.2f", total_cost);
