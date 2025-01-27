650f139925b08adb3267ae9692763d60a7585fec
bouliosOW <94017448+bouliosOW@users.noreply.github.com>
2025-01-26 20:27:45 -0500

    Add files via upload

---
+++ cpractice1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+    /* int len = 20, width = 15;
+    printf("The area of a rectangle with length %d and width %d is %d\n\n", len, width, len*width);
+
+
+    char myText[] = "Hello world!";
+    printf("%s\n", myText);
+    */
+
+    int maxScore = 123;
+    float score = 91;
+
+   printf("Max score is %d, your score is %d your percentage is %.2f", maxScore, score, score / maxScore);
+}
